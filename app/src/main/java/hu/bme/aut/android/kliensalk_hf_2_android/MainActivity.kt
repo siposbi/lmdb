2adffe70c64a9cb3aef8f5d095428eb61e843977
Balázs Sipos <thesiposthe@gmail.com>
2021-05-06 19:27:36 +0200

    Initial commit

---
+++ app/src/main/java/hu/bme/aut/android/kliensalk_hf_2_android/MainActivity.kt
@@ -0,0 +1,11 @@
+package hu.bme.aut.android.kliensalk_hf_2_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
