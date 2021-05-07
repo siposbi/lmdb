101ef70d2e37073da511039558f4dca79b5b3e81
Balázs Sipos <thesiposthe@gmail.com>
2021-05-07 20:31:27 +0200

    Added login

--- app/src/main/java/hu/bme/aut/android/kliensalk_hf_2_android/MainActivity.kt
+++ app/src/main/java/hu/bme/aut/android/kliensalk_hf_2_android/MainActivity.kt
@@ -2,17 +2,25 @@ package hu.bme.aut.android.kliensalk_hf_2_android
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import hu.bme.aut.android.kliensalk_hf_2_android.data.UserDatabase
 import hu.bme.aut.android.kliensalk_hf_2_android.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CoroutineScope by MainScope() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var database: UserDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.Theme_Kliensalkhf2android)
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        database = UserDatabase.getDatabase(applicationContext)
 
         binding.btnLogin.setOnClickListener {
             when {
@@ -25,7 +33,23 @@ class MainActivity : AppCompatActivity() {
                     binding.etPassword.error = getString(R.string.no_password_entered_error)
                 }
                 else -> {
-                    startActivity(Intent(this, ListActivity::class.java))
+                    val username = binding.etUsername.text.toString()
+                    val password = binding.etPassword.text.toString()
+                    launch {
+                        val userWithReviews =
+                            database.userWithReviewsDao().login(username, password)
+                        if (userWithReviews == null) {
+                            runOnUiThread {
+                                Toast.makeText(
+                                    applicationContext,
+                                    "Invalid Credentials",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                        } else {
+                            startActivity(Intent(this@MainActivity, ListActivity::class.java))
+                        }
+                    }
                 }
             }
         }
