6fb26fffc1c7ad82b21123ea55512bbd0a2cd463
Balázs Sipos <thesiposthe@gmail.com>
2021-05-09 21:32:02 +0200

    Code cleanup and back button

--- app/src/main/java/hu/bme/aut/android/lmdb/activity/ViewMovie.kt
+++ app/src/main/java/hu/bme/aut/android/lmdb/activity/ViewMovie.kt
@@ -1,6 +1,7 @@
 package hu.bme.aut.android.lmdb.activity
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.squareup.picasso.Picasso
 import hu.bme.aut.android.lmdb.data.model.Movie
@@ -9,8 +10,9 @@ import hu.bme.aut.android.lmdb.databinding.ActivityViewMovieBinding
 
 class ViewMovie : AppCompatActivity() {
     companion object {
-        const val KEY_REVIEW_STRING = "KEY_REVIEW_STRING"
+        const val KEY_VIEW_MOVIE_STRING = "KEY_VIEW_MOVIE_STRING"
     }
+
     private lateinit var binding: ActivityViewMovieBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,17 +20,26 @@ class ViewMovie : AppCompatActivity() {
         binding = ActivityViewMovieBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val review = intent.getParcelableExtra<Movie>(KEY_REVIEW_STRING)!!
-
-        title = review.title
+        val movie = intent.getParcelableExtra<Movie>(KEY_VIEW_MOVIE_STRING)!!
+        title = movie.title
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         val widthInPixels = applicationContext.resources.displayMetrics.widthPixels
+        Picasso.get().load(movie.posterUrl).resize(widthInPixels, 0).into(binding.ivPoster)
 
-        Picasso.get().load(review.posterUrl).resize(widthInPixels, 0).into(binding.ivPoster)
+        binding.etTitle.text = movie.title
+        binding.etYear.text = movie.year
+        binding.etGenre.text = movie.genre
+        binding.etPlot.text = movie.plot
+    }
 
-        binding.etTitle.text = review.title
-        binding.etYear.text = review.year
-        binding.etGenre.text = review.genre
-        binding.etPlot.text = review.plot
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                return true
+            }
+        }
+        return super.onContextItemSelected(item)
     }
 }
