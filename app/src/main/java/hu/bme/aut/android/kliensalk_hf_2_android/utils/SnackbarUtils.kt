217f3173d546ab1f1a86fa6e922f77be806c0267
Balázs Sipos <thesiposthe@gmail.com>
2021-05-09 18:18:00 +0200

    Fixed showLongSnackbar

--- app/src/main/java/hu/bme/aut/android/kliensalk_hf_2_android/utils/SnackbarUtils.kt
+++ app/src/main/java/hu/bme/aut/android/kliensalk_hf_2_android/utils/SnackbarUtils.kt
@@ -3,6 +3,7 @@ package hu.bme.aut.android.kliensalk_hf_2_android.utils
 import android.app.Activity
 import android.content.Context
 import android.view.View
+import com.google.android.material.snackbar.BaseTransientBottomBar.LENGTH_LONG
 import com.google.android.material.snackbar.BaseTransientBottomBar.LENGTH_SHORT
 import com.google.android.material.snackbar.Snackbar
 
@@ -11,7 +12,7 @@ fun Activity.showShortSnackbar(msg: String) {
 }
 
 fun Activity.showLongSnackbar(msg: String) {
-    showSnackBar(currentFocus ?: View(this), msg, LENGTH_SHORT)
+    showSnackBar(currentFocus ?: View(this), msg, LENGTH_LONG)
 }
 
 private fun Context.showSnackBar(view: View, msg: String, length: Int) {
