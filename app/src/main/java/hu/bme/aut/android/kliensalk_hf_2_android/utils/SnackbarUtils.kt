eeed19e92c476cc7331b0e37f7256b28bf5af835
Balázs Sipos <thesiposthe@gmail.com>
2021-05-09 18:17:18 +0200

    Initial commit

---
+++ app/src/main/java/hu/bme/aut/android/kliensalk_hf_2_android/utils/SnackbarUtils.kt
@@ -0,0 +1,19 @@
+package hu.bme.aut.android.kliensalk_hf_2_android.utils
+
+import android.app.Activity
+import android.content.Context
+import android.view.View
+import com.google.android.material.snackbar.BaseTransientBottomBar.LENGTH_SHORT
+import com.google.android.material.snackbar.Snackbar
+
+fun Activity.showShortSnackbar(msg: String) {
+    showSnackBar(currentFocus ?: View(this), msg, LENGTH_SHORT)
+}
+
+fun Activity.showLongSnackbar(msg: String) {
+    showSnackBar(currentFocus ?: View(this), msg, LENGTH_SHORT)
+}
+
+private fun Context.showSnackBar(view: View, msg: String, length: Int) {
+    Snackbar.make(view, msg, length).show()
+}
